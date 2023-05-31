a785eedbced16a9859995803ae3a721c4a26f923
mcantop <maciejcantop@icloud.com>
2023-05-31 21:04:02 +0200

    Initial Commit

---
+++ NavigationCrashCourseSwiftUI/NavigationCrashCourseSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationCrashCourseSwiftUIApp.swift
+//  NavigationCrashCourseSwiftUI
+//
+//  Created by Maciej on 31/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct NavigationCrashCourseSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
